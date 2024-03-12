2e4f2cffeada8006434c883ab3a186f6d53219d7
minhee <minhee810@naver.com>
2024-03-13 06:14:40 +0900

    Feat : 이미지 업로드 API 개발

---
+++ src/main/java/com/cos/photogramstart/domain/image/Image.java
@@ -0,0 +1,41 @@
+package com.cos.photogramstart.domain.image;
+
+import com.cos.photogramstart.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String caption;
+
+    private String postImageUrl; // 사진을 전송 받아서 서버의 특정 폴더에 저장을 할 것이고, db 에는 저장된 경로를 저장할 것임.
+
+    @JoinColumn(name = "userId")
+    @ManyToOne
+    private User user; // 연관관계의 경우 FK가 데이터 베이스에 저장됨.
+
+    // 이미지 좋아요
+
+    // 이미지 댓글
+
+    private LocalDateTime createDate;
+
+    @PrePersist // db에 insert 되기 직전에 실행
+    public void createDate() {
+        this.createDate = LocalDateTime.now();
+    }
+}
---
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -0,0 +1,41 @@
+package com.cos.photogramstart.service;
+
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import com.cos.photogramstart.domain.image.ImageRepository;
+import com.cos.photogramstart.web.dto.image.ImageUploadDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+
+    // yml 값 가져오기
+    @Value("${file.path}")
+    private String uploadFolder;
+
+    public void imageUpload(ImageUploadDto imageUploadDto, PrincipalDetails principalDetails) {
+        UUID uuid = UUID.randomUUID(); // uuid
+        String imageFileName = uuid + "_" + imageUploadDto.getFile().getOriginalFilename(); // ex) 1.jpg
+        System.out.println("이미지 파일 이름 : " + imageFileName);
+
+        Path imageFilePath = Paths.get(uploadFolder+imageFileName);
+
+        // IO, 통신 -> 예외 발생할 수 있다. 런타임 에러 예외처리
+        try {
+            Files.write(imageFilePath, imageUploadDto.getFile().getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+}
